b7ecd44884ed8a2b23c380257952d2ec0a9cf3a9
kinoppol <kinoppol@hotmail.com>
2022-01-23 11:29:11 +0700

    Update index.php

--- center/index.php
+++ center/index.php
@@ -10,54 +10,6 @@
     <title>School Web Template by devbanban.com</title>
   </head>
   <body>
-    <!--start  menu top-->
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col col-sm-12 col-md-12">
-          <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #ff7878;">
-            <a class="navbar-brand" href="#">
-              <img src="https://getbootstrap.com/docs/4.4/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
-              devbanban School
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent2">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                  <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="https://www.youtube.com/c/devbanban" target="_blank">Youtube</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="https://devbanban.com/?p=2867" target="_blank">คอร์สออนไลน์</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="https://devbanban.com/?page_id=2309" target="_blank">สนับสนุน</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="https://www.facebook.com/sornwebsites/" target="_blank">ติดต่อ</a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                  </a>
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                  </div>
-                </li>
-                
-              </ul>
-            </div>
-          </nav>
-        </div>
-      </div>
-    </div>
-    <!--end  menu -->
     <!--start  banner -->
     <div class="container">
       <div class="row">
