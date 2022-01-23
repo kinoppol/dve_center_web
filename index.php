294a0542e333080b5f5e68b6efa399d390c5758f
kinoppol <kinoppol@hotmail.com>
2022-01-23 11:17:16 +0700

    init

---
+++ index.php
@@ -0,0 +1,322 @@
+<?php
+$title='ศูนย์อาชีวศึกษาทวิภาคี';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title><?php print $title; ?></title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" type="text/css" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <style>
+            a:link{
+                color:black;
+                text-decoration:none;
+            }
+            a:hover{
+                color:darkred;
+                text-decoration:underline;
+            }
+            a:visited{
+                color:black;
+                text-decoration:none;
+            }
+        </style>
+    </head>
+    <body>
+        <!-- Navigation-->
+        <nav class="navbar navbar-light bg-light static-top">
+            <div class="container">
+                <a class="navbar-brand" href="#!"><?php print $title; ?></a>
+                <a class="btn btn-primary" href="./index.php?app/user/index">เข้าสู่ระบบ</a>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container position-relative">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6">
+                        <div class="text-center text-white">
+                            <!-- Page heading-->
+                            <h1 class="mb-5">ศูนย์อาชีวศึกษาทวิภาคี</h1>
+                            <h2>สำนักงานคณะกรรมการการอาชีวศึกษา</h2>
+                            <!-- Signup form-->
+                            <!-- * * * * * * * * * * * * * * *-->
+                            <!-- * * SB Forms Contact Form * *-->
+                            <!-- * * * * * * * * * * * * * * *-->
+                            <!-- This form is pre-integrated with SB Forms.-->
+                            <!-- To make this form functional, sign up at-->
+                            <!-- https://startbootstrap.com/solution/contact-forms-->
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Icons Grid-->
+        <section class="features-icons bg-light text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <a href="./center/index.php?c=1300000001">
+                                <img src="./images/vec.png" width="120">
+                                <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>ภาคตะวันออก 1</h3>
+                                <p class="lead mb-0">ศูนย์หลัก จังหวัดปราจีนบุรี<br>(วิทยาลัยเทคนิคปราจีนบุรี) </p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <img src="./images/vec.png" width="120">
+                            <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>ภาคตะวันออก 2</h3>
+                            <p class="lead mb-0">ศูนย์หลัก จังหวัดระยอง<br>(วิทยาลัยเทคนิคบ้านค่าย) </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <img src="./images/vec.png" width="120">
+                            <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>ภาคกลาง 1</h3>
+                            <p class="lead mb-0">ศูนย์หลัก จังหวัดพระนครศรีอยุทธยา<br>(วิทยาลัยเทคนิคพระนครศรีอยุทธยา) </p>
+                        </div>
+                    </div>
+                     <div class="col-lg-4">
+                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <img src="./images/vec.png" width="120">
+                            <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>ภาคกลาง 2</h3>
+                            <p class="lead mb-0">ศูนย์หลัก จังหวัดราชบุรี<br>(วิทยาลัยเทคนิคราชบุรี) </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <img src="./images/vec.png" width="120">
+                            <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>ภาคเหนือ 1</h3>
+                            <p class="lead mb-0">ศูนย์หลัก จังหวัดเชียงใหม่<br>(วิทยาลัยเทคนิคเชียงใหม่) </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <img src="./images/vec.png" width="120">
+                            <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>ภาคเหนือ 2</h3>
+                            <p class="lead mb-0">ศูนย์หลัก จังหวัดนครสวรรค์<br>(วิทยาลัยอาชีวศึกษานครสวรรค์) </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <img src="./images/vec.png" width="120">
+                            <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>ภาคตะวันออกเฉียงเหนือ 1</h3>
+                            <p class="lead mb-0">ศูนย์หลัก จังหวัดอุดรธานี<br>(วิทยาลัยเทคนิคอุดรธานี) </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <img src="./images/vec.png" width="120">
+                            <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>ภาคตะวันออกเฉียงเหนือ 2</h3>
+                            <p class="lead mb-0">ศูนย์หลัก จังหวัดนครราชสีมา<br>(วิทยาลัยเทคนิคนครราชสีมา) </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <img src="./images/vec.png" width="120">
+                            <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>ภาคตะวันออกเฉียงเหนือ 3</h3>
+                            <p class="lead mb-0">ศูนย์หลัก จังหวัดอุบลราชธานี<br>(วิทยาลัยเทคนิคอุบลราชธานี) </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <img src="./images/vec.png" width="120">
+                            <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>ภาคใต้ 1</h3>
+                            <p class="lead mb-0">ศูนย์หลัก จังหวัดสุราษร์ธานี<br>(วิทยาลัยเทคนิคสุราษร์ธานี) </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <img src="./images/vec.png" width="120">
+                            <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>ภาคใต้ 2</h3>
+                            <p class="lead mb-0">ศูนย์หลัก จังหวัดภูเก็ต<br>(วิทยาลัยเทคนิคภูเก็ต) </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <img src="./images/vec.png" width="120">
+                            <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>ภาคใต้ 3</h3>
+                            <p class="lead mb-0">ศูนย์หลัก จังหวัดสงขลา<br>(วิทยาลัยเทคนิคหาดใหญ่) </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <img src="./images/vec.png" width="120">
+                            <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>ปริมณฑล 1</h3>
+                            <p class="lead mb-0">ศูนย์หลัก จังหวัดปทุมธานี<br>(วิทยาลัยการอาชีวศึกษาปทุมธานี) </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <img src="./images/vec.png" width="120">
+                            <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>ปริมณฑล 2</h3>
+                            <p class="lead mb-0">ศูนย์หลัก จังหวัดสมุทรปราการ<br>(วิทยาลัยเทคนิคสมุทรปราการ) </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                    <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
+                            <img src="./images/vec.png" width="120">
+                            <h3>ศูนย์อาชีวศึกษาทวิภาคี<br>กรุงเทพมหานคร</h3>
+                            <p class="lead mb-0">ศูนย์หลักวิทยาลัยบริหารธุรกิจและการท่องเที่ยวกรุงเทพ </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Image Showcases-->
+        <section class="showcase">
+            <div class="container-fluid p-0">
+                <div class="row g-0">
+                    <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('assets/img/bg-showcase-1.jpg')"></div>
+                    <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                        <h2>Fully Responsive Design</h2>
+                        <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
+                    </div>
+                </div>
+                <div class="row g-0">
+                    <div class="col-lg-6 text-white showcase-img" style="background-image: url('assets/img/bg-showcase-2.jpg')"></div>
+                    <div class="col-lg-6 my-auto showcase-text">
+                        <h2>Updated For Bootstrap 5</h2>
+                        <p class="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 5 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 5!</p>
+                    </div>
+                </div>
+                <div class="row g-0">
+                    <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('assets/img/bg-showcase-3.jpg')"></div>
+                    <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                        <h2>Easy to Use & Customize</h2>
+                        <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Testimonials-->
+        <section class="testimonials text-center bg-light">
+            <div class="container">
+                <h2 class="mb-5">What people are saying...</h2>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                            <img class="img-fluid rounded-circle mb-3" src="assets/img/testimonials-1.jpg" alt="..." />
+                            <h5>Margaret E.</h5>
+                            <p class="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                            <img class="img-fluid rounded-circle mb-3" src="assets/img/testimonials-2.jpg" alt="..." />
+                            <h5>Fred S.</h5>
+                            <p class="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="testimonial-item mx-auto mb-5 mb-lg-0">
+                            <img class="img-fluid rounded-circle mb-3" src="assets/img/testimonials-3.jpg" alt="..." />
+                            <h5>Sarah W.</h5>
+                            <p class="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Call to Action-->
+        <section class="call-to-action text-white text-center" id="signup">
+            <div class="container position-relative">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6">
+                        <h2 class="mb-4">Ready to get started? Sign up now!</h2>
+                        <!-- Signup form-->
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- * * SB Forms Contact Form * *-->
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- This form is pre-integrated with SB Forms.-->
+                        <!-- To make this form functional, sign up at-->
+                        <!-- https://startbootstrap.com/solution/contact-forms-->
+                        <!-- to get an API token!-->
+                        <form class="form-subscribe" id="contactFormFooter" data-sb-form-api-token="API_TOKEN">
+                            <!-- Email address input-->
+                            <div class="row">
+                                <div class="col">
+                                    <input class="form-control form-control-lg" id="emailAddressBelow" type="email" placeholder="Email Address" data-sb-validations="required,email" />
+                                    <div class="invalid-feedback text-white" data-sb-feedback="emailAddressBelow:required">Email Address is required.</div>
+                                    <div class="invalid-feedback text-white" data-sb-feedback="emailAddressBelow:email">Email Address Email is not valid.</div>
+                                </div>
+                                <div class="col-auto"><button class="btn btn-primary btn-lg disabled" id="submitButton" type="submit">Submit</button></div>
+                            </div>
+                            <!-- Submit success message-->
+                            <!---->
+                            <!-- This is what your users will see when the form-->
+                            <!-- has successfully submitted-->
+                            <div class="d-none" id="submitSuccessMessage">
+                                <div class="text-center mb-3">
+                                    <div class="fw-bolder">Form submission successful!</div>
+                                    <p>To activate this form, sign up at</p>
+                                    <a class="text-white" href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                </div>
+                            </div>
+                            <!-- Submit error message-->
+                            <!---->
+                            <!-- This is what your users will see when there is-->
+                            <!-- an error submitting the form-->
+                            <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="footer bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
+                        <ul class="list-inline mb-2">
+                            <li class="list-inline-item"><a href="#!">About</a></li>
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="#!">Contact</a></li>
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="#!">Terms of Use</a></li>
+                            <li class="list-inline-item">⋅</li>
+                            <li class="list-inline-item"><a href="#!">Privacy Policy</a></li>
+                        </ul>
+                        <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2021. All Rights Reserved.</p>
+                    </div>
+                    <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
+                        <ul class="list-inline mb-0">
+                            <li class="list-inline-item me-4">
+                                <a href="#!"><i class="bi-facebook fs-3"></i></a>
+                            </li>
+                            <li class="list-inline-item me-4">
+                                <a href="#!"><i class="bi-twitter fs-3"></i></a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#!"><i class="bi-instagram fs-3"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
