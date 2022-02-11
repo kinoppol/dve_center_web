3d6a02c468c3d6f7ce6f9ba5bf2ab23fc5096335
kinoppol <kinoppol@hotmail.com>
2022-02-11 10:35:47 +0700

    news thumbnail

--- center/readNews.php
+++ center/readNews.php
@@ -10,13 +10,48 @@ if(!empty($news->attach_files)){
     print '
     <h6><i>ไฟล์แนบ</i></h6><p>';
     $files=json_decode($news->attach_files);
-
+?>
+<div class="col col-sm-9 col-md-9">
+          <div class="row">
+<?php
     foreach($files as $f){
-        ?>
-        <a href="https://dve.vec.go.th/files/news/<?php print $news->center_id.'/'.$f->location; ?>" target="_blank"><?php print $f->name; ?></a><br>
-        <?php
+        $fname=explode('.',$f->location);
+        $ext=end($fname);
+        $file_url='https://dve.vec.go.th/files/news/'.$news->center_id.'/'.$f->location;
+        if(array_search($ext,array('jpg','JPG','jpeg','JPEG','png','PNG'))){
+            ?>
+
+            <div class="col-6 col-sm-4 col-md-4" style="margin-bottom: 20px;">
+                <div class="card" style="width: 100%;">
+                  <img src="<?php print $file_url; ?>" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h6 class="card-title"><?php print $v; ?></h6>
+                    <a href="<?php print $file_url; ?>" target="_blank"><?php print $f->name; ?></a>
+                  </div>
+                </div>
+              </div>
+
+            <?php
+        }else{
+            ?>
+
+            <div class="col-6 col-sm-4 col-md-4" style="margin-bottom: 20px;">
+                <div class="card" style="width: 100%;">
+                  <img src="img/file.png" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h6 class="card-title"><?php print $v; ?></h6>
+                    <a href="<?php print $file_url; ?>" target="_blank"><?php print $f->name; ?></a>
+                  </div>
+                </div>
+              </div>
+
+            <?php
+        }
     }
-    print '</p>';
+    ?>
+          </div>
+          </div>
+<?php
 }
 
 
